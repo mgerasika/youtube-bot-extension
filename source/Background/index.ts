f1a6fe3c148795e7001d36eceaf263223e25b07f
Mykhaylo Gerasika <mger@ciklum.com>
2022-03-24 14:56:16 +0200

    Fix bugs

--- source/Background/index.ts
+++ source/Background/index.ts
@@ -1,6 +1,14 @@
 import 'emoji-log';
 import {browser} from 'webextension-polyfill-ts';
 
+// chrome.runtime.onMessage.addListener(function (msg, sender) {
+// 	// First, validate the message's structure
+// 	if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
+// 	  // Enable the page-action for the requesting tab
+// 	  chrome.pageAction.show(sender.tab.id);
+// 	}
+//   });
+
 browser.runtime.onInstalled.addListener((): void => {
   console.emoji('🦄', 'extension installed');
 });
--- source/ContentScript/index.ts
+++ source/ContentScript/index.ts
@@ -1,3 +1,31 @@
-console.log('helloworld from content script');
+console.log("helloworld from content script");
+window.addEventListener("scroll", () => {
+  console.log("hello-scroll2");
+});
+window.setTimeout(() => {
+  const root = document.body.getElementsByTagName(
+    "ytd-comment-renderer"
+  );
+  console.log("hello-result");
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.innerHTML = `.yt-img-shadow { position: relative;} .iconDiv {position:absolute;font-size:12px;border:1px solid black;border-radius:4px; z-index:100;text-transform:uppercase; font-weight:bold;background-color:white;padding:4px 8px;}`;
 
+  const div = document.createElement("div");
+  div.appendChild(style);
+  const iconDiv = document.createElement("div");
+  iconDiv.className = "iconDiv";
+  iconDiv.innerHTML = "Bot";
+  div.appendChild(iconDiv);
+  div.className = "hello";
+  div.appendChild(root[0]);
+
+  root[0].appendChild(div);
+}, 3000);
+
+// <a id="author-text" class="yt-simple-endpoint style-scope ytd-comment-renderer" href="/channel/UCRu51PFIo156tREHHVKxIDA">
+//             <span class="style-scope ytd-comment-renderer">
+//               Н
+//             </span>
+//           </a>
 export {};
